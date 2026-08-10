9695b0e68f4cf5484a6c1b0560756e6b625b7402
salma-hazem <salma.hazem.mahgoub@gmail.com>
2026-08-10 23:45:58 +0200

    added required configurations for module 5 entities ( agreement and payment )

---
+++ Mony_Loop.Infrastructure/Data/Configurations/AgreementPayment/PaymentTransactionConfiguration.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Mony_Loop.Domain.Constants.Agreement___Payment;
+using Mony_Loop.Domain.Entities.Agreement___Payment;
+
+namespace Mony_Loop.Infrastructure.Data.Configurations.AgreementPayment
+{
+    public class PaymentTransactionConfiguration
+        : IEntityTypeConfiguration<PaymentTransaction>
+    {
+        public void Configure(EntityTypeBuilder<PaymentTransaction> builder)
+        {
+            builder.ToTable("PaymentTransactions");
+
+            builder.HasKey(x => x.PaymentTransactionId);
+
+            builder.Property(x => x.MemberLedgerId)
+                .IsRequired();
+
+            builder.Property(x => x.CircleId)
+                .IsRequired();
+
+            builder.Property(x => x.RecordedByUserId)
+                .IsRequired();
+
+            builder.Property(x => x.TransactionType)
+                .IsRequired()
+                .HasMaxLength(20);
+
+            builder.Property(x => x.PaymentMethod)
+                .IsRequired()
+                .HasMaxLength(20);
+
+            builder.Property(x => x.TransactionStatus)
+                .IsRequired()
+                .HasMaxLength(20);
+
+            builder.Property(x => x.Amount)
+                .IsRequired()
+                .HasPrecision(18, 2);
+
+            builder.Property(x => x.TransactionReference)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(x => x.ReceiptNumber)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(x => x.ReceiptFilePath)
+                .IsRequired()
+                .HasMaxLength(500);
+
+            builder.Property(x => x.TransactionDate)
+                .IsRequired();
+
+            builder.Property(x => x.CreatedAt)
+                .IsRequired();
+
+            // MemberLedger 1 ---- Many PaymentTransactions
+            builder.HasOne(x => x.MemberLedger)
+                .WithMany(x => x.PaymentTransactions)
+                .HasForeignKey(x => x.MemberLedgerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Circle 1 ---- Many PaymentTransactions
+            builder.HasOne(x => x.Circle)
+                .WithMany(x => x.PaymentTransactions)
+                .HasForeignKey(x => x.CircleId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ Mony_Loop.Infrastructure/Data/MonyLoopDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mony_Loop.Domain.Entities.Agreement___Payment;
+
+namespace Mony_Loop.Infrastructure.Data
+{
+    public class MonyLoopDbContext : DbContext
+    {
+        public MonyLoopDbContext(
+            DbContextOptions<MonyLoopDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MembershipAgreement> MembershipAgreements { get; set; }
+        public DbSet<PaymentTransaction> PaymentTransactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(
+                typeof(MonyLoopDbContext).Assembly);
+        }
+    }
+}
