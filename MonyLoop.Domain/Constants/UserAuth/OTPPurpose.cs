3c8f79f1aa3f2bd8c8aaa24529d853de95f92c5a
Mahmoud Adel <adel64758@gmail.com>
2026-08-18 19:06:41 +0300

    Remove class base entity and code in class OTPPurpose

--- MonyLoop.Domain/Constants/UserAuth/OTPPurpose.cs
+++ MonyLoop.Domain/Constants/UserAuth/OTPPurpose.cs
@@ -1,32 +1,4 @@
-﻿namespace Mony_Loop.Application.Common
-{
-    public class Result
-    {
-        public bool IsSuccess { get; }
-        public Error? Error { get; }
-
-        protected Result(bool isSuccess, Error? error)
-        {
-            IsSuccess = isSuccess;
-            Error = error;
-        }
-
-        public static Result Success() => new(true, null);
-        public static Result Failure(Error error) => new(false, error);
-
-        public static Result<T> Success<T>(T value) => new(value, true, null);
-        public static Result<T> Failure<T>(Error error) => new(default, false, error);
-    }
-
-    public class Result<T> : Result
-    {
-        public T? Value { get; }
-
-        internal Result(T? value, bool isSuccess, Error? error)
-            : base(isSuccess, error)
-        {
-            Value = value;
-        }
+﻿
 namespace MonyLoop.Domain.Constants.UserAuth
 {
     public enum OTPPurpose
